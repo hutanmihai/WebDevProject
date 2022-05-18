b4838f70f849dff96eb72f9cf6b6ee5dbf2be7b9
hutanmihai <hutanmihai29@gmail.com>
2022-05-19 00:17:09 +0300

    darkmode/lightmode

---
+++ static/js/myaccount.js
@@ -0,0 +1,15 @@
+function changecolourwhite(){
+    document.documentElement.style.setProperty('--white','#222525')
+    document.documentElement.style.setProperty('--black','#f8f5f2')
+    document.documentElement.style.setProperty('--purple','#078080')
+    document.documentElement.style.setProperty('--grey','#f45d48')
+    document.documentElement.style.setProperty('--black2','#f8f5a9')
+}
+
+function changecolourdark(){
+    document.documentElement.style.setProperty('--white','#fffffe')
+    document.documentElement.style.setProperty('--black','#16161a')
+    document.documentElement.style.setProperty('--purple','#7f5af0')
+    document.documentElement.style.setProperty('--grey','#94a1b2')
+    document.documentElement.style.setProperty('--black2','#010101')
+}
